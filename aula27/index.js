ee9a80eadc78d4de930da9ffa275a96aa9a31a62
willianhenri <wh341873@gmail.com>
2024-05-10 13:45:57 -0300

    aula27

---
+++ aula27/index.js
@@ -0,0 +1,10 @@
+
+
+
+let num1 = 9.54218;
+//let num2 = Math.floor(num1);
+//let num2 = Math.ceil(num1);
+//let num2 = Math.round(num1) // arredonda para o número mais proximo
+//console.log(Math.max(1,2,3,4,5,-50,50000,405))
+//console.log(Math.min(1,2,3,4,5,-50,50000,405))
+//let aleatorio = Math.round(Math.random() * 5 + 5);
